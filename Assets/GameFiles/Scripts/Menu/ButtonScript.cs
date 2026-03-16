2dc593327269178c7a846b1880fa873f7f7f90e9
teddy0508 <guyevan0508@gmail.com>
2026-03-16 18:28:26 +0000

    Fixed Buttons

--- Assets/GameFiles/Scripts/Menu/ButtonScript.cs
+++ Assets/GameFiles/Scripts/Menu/ButtonScript.cs
@@ -3,12 +3,20 @@ using System.Collections;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class ButtonScript : MonoBehaviour, IPointerEnterHandler
+public class ButtonScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public ParticleSystem pSystem;
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         pSystem.Play();
+        gameObject.transform.localScale = new Vector3(1f, 1f, 1);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        {
+            gameObject.transform.localScale = new Vector3(0.9f, 0.9f, 1);
+        }
     }
 }
