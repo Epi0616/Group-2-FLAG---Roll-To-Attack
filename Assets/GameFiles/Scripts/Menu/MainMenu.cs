2db0d48dd27d41dff8819a794d225a67e44f1913
matthew <mgnewsham@gmail.com>
2026-04-23 23:16:08 +0100

    Revert "Merge pull request #109 from Epi0616/matt"
    This reverts commit 85a0106f660f9b9d8b962adf27f94744a7c27aaf, reversing
    changes made to 2ca8e1577f90dbe4cb346027cd7ea4aded12915d.

--- Assets/GameFiles/Scripts/Menu/MainMenu.cs
+++ Assets/GameFiles/Scripts/Menu/MainMenu.cs
@@ -1,14 +1,9 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField] private InputActionReference pauseGame;
-    [SerializeField] private SettingsUIManager SettingsUIManager;
-    [SerializeField] private GameObject greyBackground;
-
     public void PlayGame()
     {
         if (TransitionManager.instance == null)
@@ -20,49 +15,12 @@ public class MainMenu : MonoBehaviour
             TransitionManager.LoadScene("MainBuild", 0.5f, 1f);
         }
 
-    }
-
-    private void OnEnable()
-    {
-        SettingsUIManager.settingsClosed += HideGreyBackground;
-    }
-    private void OnDisable()
-    {
-        SettingsUIManager.settingsClosed -= HideGreyBackground;
-    }
-
-    private void Update()
-    {
-        if (pauseGame.action.WasPressedThisFrame())
-        {
-            if (greyBackground.activeSelf)
-            {
-                ToggleOptions();
-            }
-        }
+            
     }
 
     public void Options()
     { 
-        ToggleOptions();
-    }
-
-    private void ToggleOptions()
-    {
-        if (greyBackground.activeSelf)
-        { 
-            SettingsUIManager.ClearSettingsScreen();
-            greyBackground.SetActive(false);
-            return;
-        }
-
-        SettingsUIManager.MainSettings();
-        greyBackground.SetActive(true);
-    }
-
-    private void HideGreyBackground(bool var)
-    {
-        greyBackground.SetActive(false);
+        //add options???
     }
 
     public void Quit()
