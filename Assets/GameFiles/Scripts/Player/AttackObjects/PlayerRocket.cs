31728a1d069bb488d65be5e44f80242e6cc71355
matthew <mgnewsham@gmail.com>
2026-05-10 18:15:53 +0100

    added rocket on hit sounds

--- Assets/GameFiles/Scripts/Player/AttackObjects/PlayerRocket.cs
+++ Assets/GameFiles/Scripts/Player/AttackObjects/PlayerRocket.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerRocket : MonoBehaviour
 {
     [SerializeField] GameObject impactFieldPrefab;
+    [SerializeField] AudioClip[] rocketOnHitSounds;
     private EnemyStateController target;
     private bool searchingForTarget = false;
     private bool flyingTowardsTarget = false;
@@ -105,6 +106,7 @@ public class PlayerRocket : MonoBehaviour
         ObjectPoolManager.SpawnObject(impactFieldPrefab, groundedPosition, Quaternion.identity).GetComponent<TemporaryImpactField>().adjustObject(1f, 1f, 0.5f, 1f);
 
         Enemy.GetComponent<EnemyStateController>().OnTakeDamage(15, Color.orange);
+        AudioManager.instance.PlayRandomSoundClip(rocketOnHitSounds, transform.position);
         DestroyMe();
     }
 
