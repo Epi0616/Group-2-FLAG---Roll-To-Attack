dc3cfb2ebd319d8b51232b01ff8efd52b0e66353
teddy0508 <guyevan0508@gmail.com>
2026-05-10 18:57:38 +0100

    Added button dice

--- Assets/GameFiles/Scripts/Menu/ButtonScript.cs
+++ Assets/GameFiles/Scripts/Menu/ButtonScript.cs
@@ -5,22 +5,44 @@ using UnityEngine.EventSystems;
 
 public class ButtonScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    public Quaternion originalRotation;
+    public GameObject dice;
     //public ParticleSystem pSystem;
     public float sizeWidth;
     public float sizeHeight;
     public float sizeWidthHover;
     public float sizeHeightHover;
 
+    void Start()
+    {
+        originalRotation = Quaternion.Euler(1, 1, 1);
+        dice.SetActive(false);
+    }
+    
+    void Update()
+    {
+        ShakeDiceBody(20);
+    }
     public void OnPointerEnter(PointerEventData eventData)
     {
         //pSystem.Play();
         gameObject.transform.localScale = new Vector3(sizeWidthHover, sizeHeightHover, 1);
+        dice.SetActive(true);
+
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        {
-            gameObject.transform.localScale = new Vector3(sizeWidth, sizeHeight, 1);
-        }
+        gameObject.transform.localScale = new Vector3(sizeWidth, sizeHeight, 1);
+        dice.SetActive(false);
+    }
+
+
+    public void ShakeDiceBody(float magnitude)
+    {
+        float x = Time.time * 50f + magnitude;
+        float y = Time.time * 50f + magnitude;
+        float z = Time.time * 50f + magnitude;
+        dice.transform.rotation = originalRotation * Quaternion.Euler(x, y, z);
     }
 }
