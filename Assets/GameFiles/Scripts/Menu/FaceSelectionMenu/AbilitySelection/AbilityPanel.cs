4bfd271067ec3f839949ca52520425adc65e2e6f
matthew <mgnewsham@gmail.com>
2026-05-17 10:54:34 +0100

    smoothed out the ui
    removed button coloring and instead adjusted sizing

--- Assets/GameFiles/Scripts/Menu/FaceSelectionMenu/AbilitySelection/AbilityPanel.cs
+++ Assets/GameFiles/Scripts/Menu/FaceSelectionMenu/AbilitySelection/AbilityPanel.cs
@@ -1,17 +1,25 @@
 using System;
+using System.Collections;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class AbilityPanel : MonoBehaviour
+public class AbilityPanel : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, ISelectHandler, IDeselectHandler
 {
     public TextMeshProUGUI Name, Description;
     public GameObject AbilityHolder;
     private DraggableAbility ability;
     public static event Action<AbilityPanel> AbilitySelected;
 
+    private Coroutine sizeShiftRoutine;
+    Vector2 scaleOrigin = Vector2.one;
+
+    private void Awake()
+    {
+        scaleOrigin = transform.localScale;
+    }
     public void SetName(string name)
     { 
         Name.text = name;
@@ -41,5 +49,50 @@ public class AbilityPanel : MonoBehaviour
     public void Clicked()
     {
         AbilitySelected?.Invoke(this);
+    }
+
+    void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
+    {
+        SizeUp();
+    }
+
+    void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
+    {
+        SizeDown();
+    }
+
+    void ISelectHandler.OnSelect(BaseEventData eventData)
+    {
+        SizeUp();
+    }
+
+    void IDeselectHandler.OnDeselect(BaseEventData eventData)
+    {
+        SizeDown();
+    }
+
+    public void SizeUp()
+    {
+        if (sizeShiftRoutine != null) StopCoroutine(sizeShiftRoutine);
+        sizeShiftRoutine = StartCoroutine(SizeShiftRoutine(scaleOrigin * 1.1f));
+    }
+
+    public void SizeDown()
+    {
+        if (sizeShiftRoutine != null) StopCoroutine(sizeShiftRoutine);
+        sizeShiftRoutine = StartCoroutine(SizeShiftRoutine(scaleOrigin));
+    }
+
+    private IEnumerator SizeShiftRoutine(Vector3 targetScale)
+    {
+        float timer = 1f;
+        while (timer > 0)
+        {
+            timer -= Time.deltaTime;
+            transform.localScale = Vector3.Lerp(transform.localScale, targetScale, 1 - timer);
+            yield return null;
+        }
+
+        transform.localScale = targetScale;
     }
 }
--- Assets/GameFiles/Scripts/Menu/FaceSelectionMenu/DiceFaceSelection/DraggableAbility.cs
+++ Assets/GameFiles/Scripts/Menu/FaceSelectionMenu/DiceFaceSelection/DraggableAbility.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.Localization;
+using UnityEngine.Rendering.UI;
 using UnityEngine.UI;
 
-public class DraggableAbility : DraggableObject, IPointerEnterHandler, IPointerExitHandler
+public class DraggableAbility : DraggableObject, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler
 {
     public static event Action<LocalizedString, LocalizedString, Sprite> OnAbilityHoverStart;
     public static event Action OnAbilityHoverEnd;
@@ -13,6 +15,15 @@ public class DraggableAbility : DraggableObject, IPointerEnterHandler, IPointerE
 
     private AbilityDescriptor myAbility;
     public Image Image;
+
+    private Vector3 scaleOrigin;
+    private Coroutine sizeShiftRoutine;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        scaleOrigin = spriteObj.transform.localScale;
+    }
 
     public void SetAbilityDescriptor(AbilityDescriptor newAbility)
     {
@@ -46,10 +57,59 @@ public class DraggableAbility : DraggableObject, IPointerEnterHandler, IPointerE
         }
 
         OnAbilityHoverStart?.Invoke(name, description, sprite);
+        SizeUp();
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         OnAbilityHoverEnd?.Invoke();
+        SizeDown();
+    }
+
+    void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+    {
+        if (sizeShiftRoutine != null) StopCoroutine(sizeShiftRoutine);
+        sizeShiftRoutine = StartCoroutine(SizeShiftRoutine(scaleOrigin * 0.8f));
+    }
+
+    protected override void OnBeginDrag(PointerEventData eventData)
+    {
+        base.OnBeginDrag(eventData);
+
+        //if (sizeShiftRoutine != null) StopCoroutine(sizeShiftRoutine);
+        //sizeShiftRoutine = StartCoroutine(SizeShiftRoutine(scaleOrigin * 0.8f));
+    }
+
+    protected override void OnEndDrag(PointerEventData eventData)
+    {
+        base.OnEndDrag(eventData);
+
+        if (sizeShiftRoutine != null) StopCoroutine(sizeShiftRoutine);
+        sizeShiftRoutine = StartCoroutine(SizeShiftRoutine(scaleOrigin * 1.2f));
+    }
+
+    public void SizeUp()
+    {
+        if (sizeShiftRoutine != null) StopCoroutine(sizeShiftRoutine);
+        sizeShiftRoutine = StartCoroutine(SizeShiftRoutine(scaleOrigin * 1.2f));
+    }
+
+    public void SizeDown()
+    {
+        if (sizeShiftRoutine != null) StopCoroutine(sizeShiftRoutine);
+        sizeShiftRoutine = StartCoroutine(SizeShiftRoutine(scaleOrigin));
+    }
+
+    private IEnumerator SizeShiftRoutine(Vector3 targetScale)
+    { 
+        float timer = 1f;
+        while (timer > 0)
+        {
+            timer -= Time.deltaTime;
+            spriteObj.transform.localScale = Vector3.Lerp(spriteObj.transform.localScale, targetScale, 1 - timer);
+            yield return null;
+        }
+
+        spriteObj.transform.localScale = targetScale;
     }
 }
