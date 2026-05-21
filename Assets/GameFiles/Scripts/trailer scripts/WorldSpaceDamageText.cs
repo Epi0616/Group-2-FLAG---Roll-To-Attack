b78ebbc5637f6312bc24c56ec6b9e5ebb9f7957b
matthew <mgnewsham@gmail.com>
2026-05-21 17:56:41 +0100

    finished world space text

--- Assets/GameFiles/Scripts/trailer scripts/WorldSpaceDamageText.cs
+++ Assets/GameFiles/Scripts/trailer scripts/WorldSpaceDamageText.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using System.Threading;
 
 public class WorldSpaceDamageText : MonoBehaviour
 {
@@ -9,7 +10,8 @@ public class WorldSpaceDamageText : MonoBehaviour
     [SerializeField] private TextMeshPro tmp;
     private Camera targetCamera;
     [SerializeField] private float lifeTime = 500f;
-    private Vector3 originalScale;
+    [SerializeField] private Vector3 originalScale;
+    [SerializeField] private Vector3 targetScale;
     //private Vector3 targetWorldUp;
     //private Vector3 targetWorldPosition;
     //private Quaternion targetCameraRotation;
@@ -20,6 +22,7 @@ public class WorldSpaceDamageText : MonoBehaviour
         targetCamera = camera;
         transform.localScale = originalScale;
         StartCoroutine(DestroyRoutine());
+        StartCoroutine(ScaleUpRoutine());   
     }
 
     private void Awake()
@@ -36,20 +39,43 @@ public class WorldSpaceDamageText : MonoBehaviour
         //tmp.ForceMeshUpdate(true, true);
         //Debug.Log(tmp.mesh.bounds);
         transform.rotation = targetCamera.transform.rotation;
-        //transform.position += Vector3.up * Time.deltaTime * 3f;
-        //transform.localScale *= 0.999f;
+        transform.position += Vector3.up * Time.deltaTime * 1.5f;
+        transform.localScale *= 0.9999f;
 
         //Vector3 directionToCamera = (targetCamera.transform.position - transform.position).normalized;
         //transform.position += directionToCamera * Time.deltaTime * 5f;
 
-        Vector3 earlyCameraView = targetCamera.transform.position + new Vector3 (0, -7.5f, 10);
+        //Vector3 earlyCameraView = targetCamera.transform.position + new Vector3 (0, -7.5f, 10);
 
-        transform.position = Vector3.Lerp(transform.position, earlyCameraView, Time.deltaTime * 1f);
+        //transform.position = Vector3.Lerp(transform.position, earlyCameraView, Time.deltaTime * 1f);
+    }
+
+    private IEnumerator ScaleUpRoutine()
+    {
+        float duration = 1f;
+        float timer = 0f;
+
+        while (timer < duration)
+        { 
+            timer += Time.deltaTime;
+            transform.localScale = Vector3.Lerp(transform.localScale, targetScale, Time.deltaTime * 10f);
+            yield return null;
+        }
+        transform.localScale = targetScale;
     }
 
     private IEnumerator DestroyRoutine()
-    { 
+    {
         yield return new WaitForSeconds(lifeTime);
+
+        float timer = 1;
+
+        while (timer > 0)
+        {
+            timer -= Time.deltaTime;
+            tmp.alpha = timer;
+            yield return null;
+        }
 
         ObjectPoolManager.ReturnObjectToPool(gameObject);
     }
