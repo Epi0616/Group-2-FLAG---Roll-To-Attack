a6b17fcd3b83b52a0baa109f4b524f4ff37f7b97
Epi0616 <samuel.fyfield@outlook.com>
2026-06-02 19:46:35 +0100

    more Added to StatusSystem

--- Assets/GameFiles/refactored scrips/Entity Refactor.cs
+++ Assets/GameFiles/refactored scrips/Entity Refactor.cs
@@ -1,4 +1,7 @@
 using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 
 public enum DamageType
 {
@@ -26,17 +29,71 @@ public class TextDisplaySystem
 
 public class EntityStatusSystem
 {
+    public List<ActiveStatusEffect> currentActiveStatusEffects = new List<ActiveStatusEffect>();
+
     public void UpdateConditions()
     { 
     
     }
+
     public void OnRecieveEffect(ActiveStatusEffect statusEffect)
     {
+        // Check if the new Status can be "stacked" if not simply reset the conditions of the current Status
+        if (!newStatus.effect.isStackable)
+        {
+            for (int i = currentActiveStatusEffects.Count - 1; i >= 0; i--)
+            {
+                if (currentActiveStatusEffects[i].effect.type == newStatus.effect.type)
+                {
+                    //Debug.Log("Non Stackable Effect Found, Reseting Condition");
+                    currentActiveStatusEffects[i].ResetConditionsAll();
+                    return;
+                }
+            }
+        }
+        // Add it to the currentActiveStatusEffectsList and call the "effect added" function in the Status
+        currentStatusEffects.Add(newStatus);
+        newStatus.effect.AddEffect(this);
+
+        // The Effect Display will be handled by the Entity itself not the Status System 
 
     }
+
     public int ModifyDamage(int damageAmount, DamageType damageType)
     {
-        return damageAmount;
+        int modifiedDamageAmount = damageAmount;
+        for (int i = currentStatusEffects.Count - 1; i >= 0; i--)
+        {
+            // Call Status OnTakeDamage or equivelent trigger
+            // Can return either the adjusted value and immediately update modifiedDamageAmount 
+            // or return the delta and aggregate them applying it at the end to modify the dmg
+            // Depends if we want each status to apply its modification in isolation or have the previous effect modifications effect the next ones
+        }
+            return modifiedDamageAmount;
+    }
+
+    public void RemoveEffectByType(StatusType type)
+    {
+        for(int i = currentStatusEffects.Count - 1; i >= 0; i--)
+        {
+            // Simply Check the StatusType Enum against the desired type removing it if found
+            if (currentStatusEffects[i].effect.type == type)
+            {
+                currentStatusEffects[i].effect.RemoveEffect();
+                currentStatusEffects.RemoveAt(i);
+                //Debug.Log("Status Removed: " + type.ToString());
+            }
+        }
+
+        // RECALCULATE STATS
+        //whatever form that takes eventually
+
+    }
+
+    public void RecalculateStats()
+    {
+        // This entirely depends on where we store the Stats, if stored in here its easy to Recalculate but more difficult to use elsewhere
+        // or if stored in the Entity the StatusSystem will need someway to access and update those Stats
     }
 }
 
--- Assets/GameFiles/refactored scrips/ICondition.cs
+++ Assets/GameFiles/refactored scrips/ICondition.cs
@@ -29,10 +29,7 @@ public class HealthCondition : BaseCondition
 {
     public int healthAmount;
 
-    public HealthCondition(int currentHealth)
-    {
-        healthAmount = currentHealth;
-    }
+    public HealthCondition() { }
 
     public override void ConditionUpdate()
     {
@@ -43,5 +40,34 @@ public class HealthCondition : BaseCondition
     public override bool IsConditionMet()
     {
         return false;
+    }
+}
+
+[Serializable]
+public class DistanceCondition: BaseCondition
+{
+    private Transform targetPos;
+    private Entity entity;
+    public float distanceThreshold;
+
+    public DistanceCondition() { }
+    
+    public DistanceCondition(bool isRequired, Entity entity, Transform target, float distanceThreshold)
+    {
+        this.isRequired = isRequired;
+        targetPos = target;
+        this.entity = entity;
+        this.distanceThreshold = distanceThreshold;
+    }
+
+    public override void ConditionUpdate()
+    {
+    }
+    public override void ResetCondition()
+    {
+    }
+    public override bool IsConditionMet()
+    {
+        return (targetPos.position - entity.transform.position).magnitude < distanceThreshold;
     }
 }
