1fb2ab3d767d6acab88a0171a0628b144bcb0a2c
teddy0508 <guyevan0508@gmail.com>
2026-06-20 00:36:41 +0100

    transition rising

--- Assets/GameFiles/Scripts/TestCutscene/TestWallFade.cs
+++ Assets/GameFiles/Scripts/TestCutscene/TestWallFade.cs
@@ -13,37 +13,42 @@ public class TestWallFade : MonoBehaviour
 
     private void Start()
     {
-        meshRenderers = GetComponentsInChildren<MeshRenderer>(); //Gets all mesh renderers of child objs
-        foreach (var renderer in meshRenderers)
-        {
-            renderer.material = testMat; //Sets all materials to test mat
-        }
+        //meshRenderers = GetComponentsInChildren<MeshRenderer>(); //Gets all mesh renderers of child objs
+        //foreach (var renderer in meshRenderers)
+        //{
+        //    renderer.material = testMat; //Sets all materials to test mat
+        //}
 
-        a = 0;
-        checkMat = false;
+        //a = 0;
+        //checkMat = false;
     }
 
     private void Update()
     {
-        if (a < 1f)
+        transform.position = new Vector3(transform.position.x, transform.position.y + Time.deltaTime * seconds, transform.position.z); //Moves wall back each frame
+        if(transform.position.y > 11f)
         {
-            foreach (var renderer in meshRenderers) //Increases alpha of every mat each frame
-            {
-                renderer.material.SetFloat("Alpha", a);
-
-                Debug.Log(renderer.material.GetFloat("Alpha"));
-            }
-
-            a += Time.deltaTime / seconds;
+            transform.position = new Vector3(14, 11, -17); //Stops wall at y = 11
         }
-        else if (!checkMat)
-        {
-            foreach (var renderer in meshRenderers) //Sets all mats to normal mat
-            {
-                renderer.material = normalMat;
+        //if (a < 1f)
+        //{
+        //    foreach (var renderer in meshRenderers) //Increases alpha of every mat each frame
+        //    {
+        //        renderer.material.SetFloat("Alpha", a);
 
-                checkMat = true;
-            }
-        }
+        //        Debug.Log(renderer.material.GetFloat("Alpha"));
+        //    }
+
+        //    a += Time.deltaTime / seconds;
+        //}
+        //else if (!checkMat)
+        //{
+        //    foreach (var renderer in meshRenderers) //Sets all mats to normal mat
+        //    {
+        //        renderer.material = normalMat;
+
+        //        checkMat = true;
+        //    }
+        //}
     }
 }
