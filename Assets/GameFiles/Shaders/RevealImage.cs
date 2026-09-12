c28949ac39a73001d2c2ef6f3aaed504e47ad904
Epi0616 <samuel.fyfield@outlook.com>
2026-09-12 19:25:58 +0100

    New Spell Sigil Added

--- Assets/GameFiles/Shaders/RevealImage.cs
+++ Assets/GameFiles/Shaders/RevealImage.cs
@@ -4,6 +4,7 @@ public class RevealImage : Image
 {
     [SerializeField,Range(0f, 1f)]
     private float revealProgress = 0f;
+    private Color sigilColour = Color.red;
     private float randomOffset;
     public float RevealProgress
     {
@@ -20,10 +21,21 @@ public class RevealImage : Image
         }
     }
 
+    public Color SigilColour
+    {
+        get => sigilColour;
+        set
+        {
+            sigilColour = value;
+            SetVerticesDirty();
+        }
+    }
+
     protected override void Awake()
     {
         base.Awake();
         revealProgress = 0;
+        sigilColour = Color.red;
         randomOffset = Random.Range(-1f, 1f);
         if (randomOffset < 0.5f)
         {
@@ -45,12 +57,19 @@ public class RevealImage : Image
         for (int i = 0; i < toFill.currentVertCount; i++)
         {
             toFill.PopulateUIVertex(ref vertex, i);
-
+            
             Vector4 uv1 = vertex.uv1;
             uv1.x = revealProgress;
             uv1.y = randomOffset;
             vertex.uv1 = uv1;
-           // Debug.Log("UV0: " + vertex.uv1.x);
+
+            Vector4 uv3 = vertex.uv3;
+            uv3.x = sigilColour.r;
+            uv3.y = sigilColour.g;
+            uv3.z = sigilColour.b;
+            vertex.uv3 = uv3;
+            //Debug.Log("sigilColour: " + sigilColour);
+           // Debug.Log("UV Colours: " + uv3.x + " , " + uv3.y + " , " + uv3.z);
             toFill.SetUIVertex(vertex, i);
         }
     }
